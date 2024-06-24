c60953411b1b1e05ede543f6ffa8482e7e057e8a
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-24 11:21:04 +0530

    Added cards to display book information and buttons to add/delete books

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const myLibrary = []
 
-function Book(title, author, pages, read){
+function Book(id, title, author, pages, read){
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -21,3 +22,22 @@ function addBookToLibrary(bookObject){
 
 // const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "no");
 // console.log(theHobbit.info());
+
+
+const addButton = document.querySelector(".add-btn");
+const deleteButton = document.querySelector(".delete-btn");
+const fetchBookTitle = document.querySelector("#title");
+const fetchBookAuthor = document.querySelector("#author");
+const fetchBookPages = document.querySelector("#pages");
+const fetchBookRead = document.querySelector("input[name=read]:checked");
+let bookObject, bookTitleValue, bookAuthorValue, bookPagesValue, bookReadValue, id = 0 ;
+
+addButton.addEventListener('click', () => {
+    bookTitleValue = fetchBookTitle.value;
+    bookAuthorValue = fetchBookAuthor.value;
+    bookPagesValue = fetchBookPages.value;
+    bookReadValue = fetchBookRead.value;
+    id++;
+    bookObject = new Book(id,bookTitleValue, bookAuthorValue, bookPagesValue, bookReadValue);
+    myLibrary.push(bookObject);
+})
